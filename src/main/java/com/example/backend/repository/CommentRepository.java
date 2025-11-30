70155109ef7aa23c575a15654919f1281b16d072
jihyeonjeong <jh.jeong129@gmail.com>
2025-11-30 16:31:29 +0000

    post_comment_update_delete done.

--- src/main/java/com/example/backend/repository/CommentRepository.java
+++ src/main/java/com/example/backend/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package com.example.backend.repository;
 
 import com.example.backend.entity.Comment;
+import com.example.backend.entity.enums.CommentStatus;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +10,8 @@ import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID>, CommentRepositoryCustom {
     
-    List<Comment> findByPost_PostIdOrderByCreatedAtAsc(UUID postId);
+    List<Comment> findByPost_PostIdAndStatusAndDeletedAtIsNullOrderByCreatedAtAsc(
+        UUID postId,
+        CommentStatus status
+    );
 }
